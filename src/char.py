6962e54496766f08542b7c4934f368237656aac4
JLPBoss <jettpennington@gmail.com>
2023-10-26 14:54:25 -0400

    done with itteration 1

--- src/char.py
+++ src/char.py
@@ -5,14 +5,19 @@ class Char:
     def __init__(self, name, alignment, mod_arr):
         self.name = name
         self.alignment = alignment
-        self.hp = 5
-        self.ac = 10
         self.str = mod(mod_arr[0], 0)
         self.dex = mod(mod_arr[1], 0)
         self.con = mod(mod_arr[2], 0)
         self.wis = mod(mod_arr[3], 0)
         self.int = mod(mod_arr[4], 0)
         self.riz = mod(mod_arr[5], 0)
+        self.hp = 5 + self.con.mod
+        self.ac = 10 + self.dex.mod
+        self.xp = 0
+        self.lvl = (self.xp // 1000) + 1
+
+        if self.hp < 1:
+            self.hp = 1
 
 
     def attack(self, roll, enemy, to_hit_mod):
@@ -24,7 +29,18 @@ class Char:
             return False
         
     def calcDamage(self, roll, enemy, to_hit_mod, damage_mod, damage_roll): 
-        if self.attack(roll, enemy, to_hit_mod):
+        if self.attack(roll, enemy, to_hit_mod + self.lvl // 2):
             if(roll == 20):
                 damage_roll = damage_roll * 2
-            enemy.hp -= (damage_roll + damage_mod) 
+            total_dmg = (damage_roll + damage_mod)
+            if total_dmg <= 1:
+                total_dmg = 1
+            enemy.hp -= total_dmg
+            self.testLvl()
+
+    def testLvl(self):
+        self.xp += 10
+        if self.lvl != (self.xp // 1000) + 1:
+            self.hp += 5 + self.con.mod
+        self.lvl = (self.xp // 1000) + 1
+        
--- tests/test_char.py
+++ tests/test_char.py
@@ -11,12 +11,12 @@ def test_Alignment():
     assert charactor.alignment is not None
 
 def test_Health():
-    charactor = Char("jett", "lga", [10, 10, 10, 10, 10, 10])
-    assert charactor.hp == 5
+    charactor = Char("jett", "lga", [10, 10, 1, 10, 10, 10])
+    assert charactor.hp == 1
 
 def test_Armor():
-    charactor = Char("jett", "lga", [10, 10, 10, 10, 10, 10])
-    assert charactor.ac == 10
+    charactor = Char("jett", "lga", [10, 16, 10, 10, 10, 10])
+    assert charactor.ac == 13
 
 def test_Attack(): 
     charactor = Char("jett", "lga", [10, 10, 10, 10, 10, 10])
@@ -24,38 +24,65 @@ def test_Attack():
     assert type(charactor.attack(15, charactor2, 3)) == bool
 
 def test_calc_dmg():
-    charactor = Char("jett", "lga", [10, 10, 10, 10, 10, 10])
+    charactor = Char("jett", "lga", [1, 10, 10, 10, 10, 10])
     charactor2 = Char("will", "Lg", [10, 10, 10, 10, 10, 10])
-    charactor.calcDamage(19, charactor2, 1, 0, 1)
+    charactor.calcDamage(19, charactor2, charactor.str.mod, charactor.str.mod, 1)
     assert charactor2.hp == 4
 
 def test_crit():
-    charactor = Char("jett", "lga", [10, 10, 10, 10, 10, 10])
+    charactor = Char("jett", "lga", [16, 10, 10, 10, 10, 10])
     charactor2 = Char("will", "Lg", [10, 10, 10, 10, 10, 10])
     charactor2.ac = 100
-    charactor.calcDamage(20, charactor2, 1, 0, 1)
-    assert charactor2.hp == 3
+    charactor.calcDamage(20, charactor2, charactor.str.mod, charactor.str.mod, 1)
+    assert charactor2.hp == 0
 
 def test_str(): 
     charactor = Char("jett", "lga", [10, 10, 10, 10, 10, 10])
-    assert charactor.str is not None
+    assert charactor.str.mod == 0
 
 def test_dex(): 
-    charactor = Char("jett", "lga", [10, 10, 10, 10, 10, 10])
-    assert charactor.dex is not None
+    charactor = Char("jett", "lga", [10, 1, 10, 10, 10, 10])
+    assert charactor.dex.mod == -5
 
 def test_con(): 
-    charactor = Char("jett", "lga", [10, 10, 10, 10, 10, 10])
-    assert charactor.con is not None
+    charactor = Char("jett", "lga", [10, 10, 20, 10, 10, 10])
+    assert charactor.con.mod == 5
 
 def test_wis(): 
-    charactor = Char("jett", "lga", [10, 10, 10, 10, 10, 10])
-    assert charactor.wis is not None
+    charactor = Char("jett", "lga", [10, 10, 10, 11, 10, 10])
+    assert charactor.wis.mod == 0
 
 def test_int(): 
-    charactor = Char("jett", "lga", [10, 10, 10, 10, 10, 10])
-    assert charactor.int is not None
+    charactor = Char("jett", "lga", [10, 10, 10, 10, 7, 10])
+    assert charactor.int.mod == -2
 
 def test_riz(): 
+    charactor = Char("jett", "lga", [10, 10, 10, 10, 10, 12])
+    assert charactor.riz.mod == 1
+
+def test_xp():
+    charactor = Char("jett", "lga", [1, 10, 10, 10, 10, 10])
+    charactor2 = Char("will", "Lg", [10, 10, 10, 10, 10, 10])
+    charactor.calcDamage(19, charactor2, charactor.str.mod, charactor.str.mod, 1)
+    assert charactor.xp == 10
+
+def test_lvl():
+    charactor = Char("jett", "lga", [1, 10, 10, 10, 10, 10])
+    charactor.xp = 1000
+    charactor.testLvl()
+    assert charactor.lvl == 2
+
+# From here
+def test_extra_lvl_hp():
+    charactor = Char("jett", "lga", [1, 10, 10, 10, 10, 10])
+    charactor.xp = 1000
+    charactor.testLvl()
+    assert charactor.hp == 10
+
+def test_extra_dmg():
     charactor = Char("jett", "lga", [10, 10, 10, 10, 10, 10])
-    assert charactor.riz is not None
+    charactor2 = Char("will", "Lg", [10, 10, 10, 10, 10, 10])
+    charactor.xp = 4000
+    charactor.testLvl()
+    charactor.calcDamage(8, charactor2, charactor.str.mod, charactor.str.mod, 1)
+    assert charactor2.hp == 4
